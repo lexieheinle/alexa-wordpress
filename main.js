240c29d05c8a9eb45cad8390eafad05e8377a6ba
Lexie Heinle <lexie.heinle@gmail.com>
2017-02-24 16:33:01 -0500

    Add category posts

--- main.js
+++ main.js
@@ -3,14 +3,30 @@ var news = require('./lib/news.js');
 
 exports.handler = function (event, context) {
   'use strict';
-  news.getRecent().then(function(feed_response) {
-    context.succeed({
-  		statusCode: 200,
-  		headers: { 'Content-Type': 'application/json' },
-  		body: JSON.stringify(feed_response),
-  	});
-  }).catch(function(reason) {
-    context.fail('Unable to get news. Try again later.' + reason)
-  });
+  //gusto 2610, latest sports 23847 local-news 30
+  console.log(event)
+  console.log(context)
+  if (event.path == '/category') {
+    var category_id = event.queryStringParameters.category_id;
+    news.getCategory(category_id).then(function(feed_response) {
+      context.succeed({
+    		statusCode: 200,
+    		headers: { 'Content-Type': 'application/json' },
+    		body: JSON.stringify(feed_response),
+    	});
+    }).catch(function(reason) {
+      context.fail('Unable to get news. Try again later.' + reason)
+    });
+  } else {
+    news.getRecent().then(function(feed_response) {
+      context.succeed({
+    		statusCode: 200,
+    		headers: { 'Content-Type': 'application/json' },
+    		body: JSON.stringify(feed_response),
+    	});
+    }).catch(function(reason) {
+      context.fail('Unable to get news. Try again later.' + reason)
+    });
+  }
 
 };
